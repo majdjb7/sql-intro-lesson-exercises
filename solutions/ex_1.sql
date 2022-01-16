a5b4106dbfc70a0b7e44f4ccd711ba0b2b107c00
majdjb7 <majdjd171@gmail.com>
2022-01-16 17:24:38 +0200

    Exercises complete

--- solutions/ex_1.sql
+++ solutions/ex_1.sql
@@ -1,1 +1,29 @@
 USE sql_intro;
+
+-- CREATE TABLE Dolphin (
+--     name VARCHAR(30) NOT NULL PRIMARY KEY,
+--     color VARCHAR(30),
+--     height INT(10),
+--     healthy BIT NOT NULL DEFAULT 1
+-- );
+
+-- INSERT INTO Dolphin (name, color, height)
+-- VALUES("Daron", "gray", 5);
+
+
+-- INSERT INTO Dolphin (name, color, height)
+-- VALUES("Harry", "yellow", 1);
+
+
+-- INSERT INTO Dolphin (name, color, height)
+-- VALUES("Dan", "blue", 1);
+
+
+-- INSERT INTO Dolphin (name, color, height)
+-- VALUES("Yuval", "red", 3);
+
+
+-- INSERT INTO Dolphin (name, color, height)
+-- VALUES("Dorit", "green", 3);
+
+select * from Dolphin WHERE height > 2;
--- solutions/ex_6.sql
+++ solutions/ex_6.sql
@@ -1,1 +1,6 @@
 USE sql_intro;
+
+SELECT name, height
+FROM Dolphin
+WHERE healthy=1
+ORDER BY height DESC
